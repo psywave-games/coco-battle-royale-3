4d2d0e0d18764cd2be662efe505fa8ac3d9ee228
RodrigoDornelles <rodrigo@dornelles.me>
2020-10-17 14:00:09 -0300

    add pause & reset button

--- simple_game.c
+++ simple_game.c
@@ -21,20 +21,40 @@ int main(void)
     InitWindow(screenWidth, screenHeight, GAME_TITLE);
     SetTargetFPS(1000);            
 
-    for (char i = 0; i < MAX_PLAYERS; PlayerInit(i), i++);
 
     while (!WindowShouldClose()) 
     {
-        InputStep();
-        for (char i = 0; i < MAX_PLAYERS; PlayerStep(i), i++);
+        static bool pause = false;
+        static bool reset = true;
+        
+        // RESET GAME
+        if (reset) {
+            for (char i = 0; i < MAX_PLAYERS; PlayerInit(i), i++);
+            pause = false;
+            reset = false;
+        }
 
+        // PRE-STEP GAME
+        InputStep();
+
+        // STEP GAME
+        if (!pause) {
+            for (char i = 0; i < MAX_PLAYERS; PlayerStep(i), i++);
+        }
+
+        // DRAW GAME
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawFPS(0,0);
-
+        DrawFPS(10,0);
         for (char i = 0; i < MAX_PLAYERS; PlayerDraw(i), i++);
-        
+        if (pause){
+            DrawText("PAUSED!", 10, 64, 32, WHITE);
+        }
         EndDrawing();
+
+        // END STEP
+        pause = IsKeyPressed(KEY_P)? !pause: pause;
+        reset = IsKeyPressed(KEY_R);
     }
 
     CloseWindow();   
