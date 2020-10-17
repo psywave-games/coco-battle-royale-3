7025562bd6b4aa1b5c9de2b84a26073563b65321
RodrigoDornelles <rodrigo@dornelles.me>
2020-10-17 15:09:18 -0300

    init player render

--- src/header.h
+++ src/header.h
@@ -6,13 +6,16 @@
 #define PLAYER_ATTACK_TIME              (1)
 #define PLAYER_ANY                      (-1)
 #define PLAYER_AWAY                     (3)
-#define PLAYER_SIZE                     (32)
+#define PLAYER_SIZE                     (24)
 #define PLAYER_MAX_SPEED                (500)
 #define PLAYER_INC_SPEED                (0.96)
 #define WORD_LIMIT_X                    (DEFAULT_SCREEN_WIDTH-PLAYER_SIZE-1)
 #define WORD_LIMIT_Y                    (DEFAULT_SCREEN_HEIGHT-PLAYER_SIZE-1)
 #define PointDistance2D(x1,y1,x2,y2)    sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)))
+#define BOOL_SIGN(sign)                 (sign?-1:1) 
 #define UNIX_TIME                       time(NULL)
+#define SIDE_LEFT                       false
+#define SIDE_RIGHT                      true
 
 void InputStep();
 int InputAxisX();
--- src/player.c
+++ src/player.c
@@ -15,6 +15,7 @@ static struct player_t {
     float y;
     float hspeed;
     float vspeed;
+    bool sign;
     unsigned char state;
     time_t last_attack;
 }   player[MAX_PLAYERS];
@@ -49,13 +50,49 @@ void PlayerInit(char player_id)
 
 void PlayerDraw(char player_id)
 {
-    DrawRectangle(
-        player[player_id].x, 
-        player[player_id].y,
-        PLAYER_SIZE,
-        PLAYER_SIZE,
-        WHITE
-    );
+    static signed char player_ancor_x = 8;
+    static signed char player_ancor_y = 12;
+    static signed char trinagle_primary_x1 = 5;
+    static signed char trinagle_primary_y1 = 10;
+    static signed char trinagle_primary_x2 = 5;
+    static signed char trinagle_primary_y2 = 20;
+    static signed char trinagle_primary_x3 = 14;
+    static signed char trinagle_primary_y3 = 10;
+    static signed char trinagle_secondary_x1 = 0;
+    static signed char trinagle_secondary_y1 = 10;
+    static signed char trinagle_secondary_x2 = 3;
+    static signed char trinagle_secondary_y2 = 16;
+    static signed char trinagle_secondary_x3 = 3;
+    static signed char trinagle_secondary_y3 = 10;
+    static signed char trinagle_third_x1 = 3;
+    static signed char trinagle_third_y1 = 6;
+    static signed char trinagle_third_x2 = 0;
+    static signed char trinagle_third_y2 = 8;
+    static signed char trinagle_third_x3 = 3;
+    static signed char trinagle_third_y3 = 8;
+    static signed char square_head_x = 12;
+    static signed char square_head_y = 1;
+
+    // Triangle Primary|Seecondary| Point A|B|C
+    Vector2 tppa = {player[player_id].x + ((trinagle_primary_x1  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_primary_y1 - player_ancor_y};
+    Vector2 tppb = {player[player_id].x + ((trinagle_primary_x2  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_primary_y2 - player_ancor_y};
+    Vector2 tppc = {player[player_id].x + ((trinagle_primary_x3  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_primary_y3 - player_ancor_y};
+    Vector2 tspa = {player[player_id].x + ((trinagle_secondary_x1  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_secondary_y1 - player_ancor_y};
+    Vector2 tspb = {player[player_id].x + ((trinagle_secondary_x2  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_secondary_y2 - player_ancor_y};
+    Vector2 tspc = {player[player_id].x + ((trinagle_secondary_x3  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_secondary_y3 - player_ancor_y};
+    Vector2 ttpa = {player[player_id].x + ((trinagle_third_x1  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_third_y1 - player_ancor_y};
+    Vector2 ttpb = {player[player_id].x + ((trinagle_third_x2  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_third_y2 - player_ancor_y};
+    Vector2 ttpc = {player[player_id].x + ((trinagle_third_x3  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + trinagle_third_y3 - player_ancor_y};
+
+    /// Square Head Point|Size
+    Vector2 shp = {player[player_id].x + ((square_head_x  - player_ancor_x) * BOOL_SIGN(player[player_id].sign)), player[player_id].y + square_head_y - player_ancor_y};
+    Vector2 shs = {3, 6};
+
+    /// Render
+    DrawTriangle(tppa, tppb, tppc, WHITE);
+    DrawTriangle(tspa, tspb, tspc, WHITE);
+    DrawTriangle(ttpa, ttpb, ttpc, WHITE);
+    DrawRectangleV(shp, shs, WHITE);
 }
 
 void PlayerStep(char player_id)
@@ -81,6 +118,7 @@ void PlayerStep(char player_id)
     }
     else if (axis_x != 0 || axis_y != 0) {
         player[player_id].state = fsm_player_walk;
+        player[player_id].sign = axis_x < 0;
     }
     else {
         player[player_id].state = fsm_player_idle;
